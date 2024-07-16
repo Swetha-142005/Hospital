85b28f8cff77e20828fa53029d5c65e742e5f237
Swetha-142005 <swethathangaraj2005@gmail.com>
2024-07-16 16:42:34 +0530

    Add files via upload

---
+++ Hospital/app.js
@@ -0,0 +1,5 @@
+const bookingsuccess=document.getElementById("booking-success");
+function book(){
+    bookingsuccess.innerHTML="Booked Successfully!";
+    bookingsuccess.style.display="block";
+}
